9351162f30d4661a7162db44a3f9caaf0ba43412
Noel Hagos <noelhagos@MacBook-Pro.local>
2025-01-26 16:34:41 -0500

    APP

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 // require("dotenv").config();
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
