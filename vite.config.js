24f1f343fc75ef3e6863e168160a0f2cf4e0dd62
Noel Hagos <noelhagos@MacBook-Pro.local>
2025-01-26 10:23:26 -0500

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "/netflix",
-})
 
-module.exports = {  
-  // Other configurations...  
   module: {  
     rules: [  
       {  
@@ -22,4 +21,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // or your designated output folder  
     publicPath: '/your-repo-name/', // Adjust this to your repo name  
   },  
-};  
+
+})
+
